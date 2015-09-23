39f78b8aa138c689410cd5b25b9ccc88554262f2
wrt6000 <wrt6000@hotmail.com>
2015-09-22 21:38:41 -0500

    cambio en el database.yml

--- tbc/config/routes.rb
+++ tbc/config/routes.rb
@@ -1,9 +1,74 @@
 Rails.application.routes.draw do
+
+
+
+  get 'reservas/crear' => 'reserva#create', :as => 'reserva_cree'
+  get 'reserva/' => 'reserva#index'
+
+  #resources se encarga de manejar las rutas del CRUD
+  resources :vcubs
+  #no tocar estas 3 rutas
+  put 'vcubs/:id/posicion' => 'vcubs#cambiar_posicion'
+  put 'vcubs/:id/estado' => 'vcubs#cambiar_estado'
+  put 'vcubs/:id/prestada' => 'vcubs#cambiar_prestada'
+
+  get 'administrador' => 'usuarios#administrador'
+
+  get 'emergencia/' => 'emergencia#index'
+  get 'emergencia/create'
+  get 'emergencia/show'
+  get 'emergencia/destroy'
+
+
+  resources :tranvia do
+
+    resources :reportets
+  end
+
+  put 'tranvia/:id/posicion' => 'tranvia#cambiar_posicion'
+  put 'tranvia/:id/estado' => 'tranvia#cambiar_estado'
+  post 'tranvia/:id/emergencia' => 'tranvia#emergencia'
+  post 'tranvia/:id/reportar' => 'tranvia#reporte'
+
+
+
+  get 'reportets/' => 'reportets#index', :as =>'reportet'
+  get 'reportets/create'
+
+  get 'reportems/' => 'reportems#index', :as =>'reportem'
+  get 'reportems/create'
+
+
+  
+  resources :mobibuses do
+    resources :reportems
+  end
+  put 'mobibuses/:id/posicion'  => 'mobibuses#cambiar_posicion'
+  put 'mobibuses/:id/estado'  => 'mobibuses#cambiar_estado'
+  post 'mobibuses/:id/reportar' => 'mobibuses#reporte'
+  resources :usuarios
+  post 'usuarios/:id/reserva' => 'usuarios#reservar'
+
+
+  resources :conductors
+
+  post 'conductors/:id/asignarm' => 'conductors#asignarm'
+
+  post 'conductors/:id/asignart' => 'conductors#asignart'
+
+  get 'reportets/create' => 'reportets#create', :as => 'reportets_crear'
+  get 'reportems/create' => 'reportems#create', :as => 'reportems_crear'
+
+  resources :reportets
+
+  resources :reportems
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'usuarios#access'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
